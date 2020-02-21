286d49bd9e42c70ef95d76dc5dfdf75bb651f013
Suketu Patel <suketupatel29@gmail.com>
2020-02-21 17:58:26 +0530

    Add files via upload

---
+++ docs/precache-manifest.2a956f635d7a14d2aaabbb02efdcdb1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "838eff1b65de58807844813f7f70a8e2",
+    "url": "/reminderApp-serve/index.html"
+  },
+  {
+    "revision": "75112c2de389ed6fb35b",
+    "url": "/reminderApp-serve/static/css/2.53964a50.chunk.css"
+  },
+  {
+    "revision": "75112c2de389ed6fb35b",
+    "url": "/reminderApp-serve/static/js/2.4195fa90.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/reminderApp-serve/static/js/2.4195fa90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e79706184888292a31c",
+    "url": "/reminderApp-serve/static/js/main.111a6d05.chunk.js"
+  },
+  {
+    "revision": "6d6ba45c7dc0bd5115e9",
+    "url": "/reminderApp-serve/static/js/runtime-main.81ba3426.js"
+  }
+]);
