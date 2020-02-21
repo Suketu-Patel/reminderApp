723d046987e8c05d74b16b8baf0f5f031ce5d003
Suketu Patel <suketupatel29@gmail.com>
2020-02-21 22:08:24 +0530

    Add files via upload

---
+++ docs/precache-manifest.48ba4794972c2fbc29992c0bc4e9509e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944ebce629aa2bdd05097d578c925d70",
+    "url": "/reminderApp/index.html"
+  },
+  {
+    "revision": "d27b7d2df89e043523eb",
+    "url": "/reminderApp/static/css/2.c595a105.chunk.css"
+  },
+  {
+    "revision": "d27b7d2df89e043523eb",
+    "url": "/reminderApp/static/js/2.bdde4b83.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/reminderApp/static/js/2.bdde4b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94cc082a29e98cc72fe0",
+    "url": "/reminderApp/static/js/main.5ddd1e68.chunk.js"
+  },
+  {
+    "revision": "78094f05edf462420040",
+    "url": "/reminderApp/static/js/runtime-main.71ea0741.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.7e50b7f5f0d9a934c0d8748c77d45be4.js"
+  "/reminderApp/precache-manifest.48ba4794972c2fbc29992c0bc4e9509e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reminderApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
