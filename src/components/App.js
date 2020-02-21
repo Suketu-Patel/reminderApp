ad37a84d8eac4c1173135d81d902ce55c50d09d8
Suketu Patel <suketupatel29@gmail.com>
2020-02-21 23:39:30 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReminderForm from './ReminderForm';
 import ReminderFooter from './ReminderFooter';
 import RemidnerList from './ReminderList';
-import { Switch,Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ReminderEdit from './ReminderEdit';
+import ReminderHeader from './ReminderHeader';
 
 const App = () => {
     return (
-        <div className="container">
+        <div style={{height:100+"%"}} >
             <Switch>
                 <Route path="/" exact>
-                    <ReminderForm />
-                    <RemidnerList />
-                    <ReminderFooter />
+                    <ReminderHeader />
+                    <div className="h-100 container">
+                        <ReminderForm />
+                        <RemidnerList />
+                        <ReminderFooter />
+                    </div>
+
                 </Route>
                 <Route path="/edit/:reminderId">
-                    <ReminderEdit/>
+                    <ReminderEdit />
                 </Route>
             </Switch>
 
